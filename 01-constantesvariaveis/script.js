c5f4c54c66d2949a3c695ba824d19dbc03de01af
JOAO VICTOR SANTOS DE ALBUQUERQUE <joao.vsalbuquerque1@senacsp.edu.br>
2026-02-24 11:37:36 -0300

    aula24/2

---
+++ 01-constantesvariaveis/script.js
@@ -0,0 +1,63 @@
+//comentário em uma linha
+
+/*comentário
+em
+varias
+linhas */
+
+//exibindo um numero na tela
+console.log(2);
+
+//exibindo o tipo de dado na tela
+console.log(typeof 2.5)
+
+//imprimindo uma constante 
+const meunumero = 10;
+console.log(meunumero)
+
+//string utilizando aspas simples e aspas duplas
+console.log('Olá, Mundo!')
+console.log("Olá planeta terra")
+
+const nome = "João Victor";
+console.log(nome);
+
+//Contagem de String
+console.log(nome.length);
+
+
+//String de números 
+const valores = "123";
+console.log(valores);
+console.log(typeof valores)
+
+//pular uma linha na String 
+console.log("Pular uma linha para mostrar o resultado \nR$125,00");
+
+//espaçamento na String
+console.log("Dar espaço \tno texto");
+
+//Boolean verdadeiro ou falso
+const noite = true;
+const tarde = false;
+console.log(noite);
+console.log(tarde);
+
+const maioridade = 18;
+const maiordeidade = maioridade > 17;
+console.log(maiordeidade)
+
+//variavel de numero flutuante
+
+let preco = 19.99;
+preco = 21.99;
+console.log(preco);
+
+//undefined (variavel declarada mas não inicializada)
+let x;
+console.log(x);
+
+//Null (representa ausencia internacional de valor)
+let resposta = null;
+console.log(resposta);
+console.log(typeof resposta); 
---
+++ 02-concatenacaointerpolacao/script.js
@@ -0,0 +1,15 @@
+//Concatenação 
+const meutexto = "Olá " + "Mundo";
+console.log(meutexto);
+
+const numeronovo = 15;
+console.log("O número é " + numeronovo);
+
+const meutexto2 = "Olá "  .concat("mundo novamente");
+console.log(meutexto2);
+
+//Interpolando uma string, um numero e uma expressão
+const primeironome = "João";
+const idade = 30
+console.log(`Olá, ${primeironome}, você tem ${idade} anos.`); 
+
